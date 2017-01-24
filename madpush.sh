b880494a3cec35288cb7be55684cb1f54ab12aae
joanbruna <bruna@cims.nyu.edu>
2017-01-23 23:12:27 -0500

    update

--- madpush.sh
+++ madpush.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 messg=${1:-"update"}
+alias pubdir="cd public"
 
 #first push private changes
 git pull && git add --all && git commit -m "$messg" && git push --set-upstream origin master;
@@ -10,7 +11,7 @@ hugo ;
 
 #push public site
 #cd public && (cd ..; hugo) && git add --all & git commit -m "$messg" && git push -f origin gh-pages ;
-cd public  
+pubdir 
 
 
 
