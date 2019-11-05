d632ee8111f4ef20521f88d033dc48aabed982fb
alexwein <alexander.wein@gmail.com>
2019-11-05 11:20:48 -0500

    update

---
+++ madpush-pause.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+messg=${1:-"update"}
+alias pubdir="cd public"
+
+#first push private changes
+git pull && git add --all && git commit -m "$messg" && git push --set-upstream origin master;
+
+#create public site
+hugo ;
+
+#push public site
+pubdir && git pull && git add --all && pwd && git commit -m "$messg" && git push -f origin gh-pages ;
+
+pause
